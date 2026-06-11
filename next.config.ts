e29b9dfe4a8c39fdffaedf603e53cb52b89aa973
Brian Blankenship <c0dezer019@zohomail.com>
2026-06-11 15:01:46 -0500

    chore: confiig update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/server/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? 'http://127.0.0.1:8000/server/:path*'
+          : '/server/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
